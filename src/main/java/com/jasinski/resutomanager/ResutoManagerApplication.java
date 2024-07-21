4589c7b7830db238b6c3f3279abb5e77bb97ead7
Szymon Jasiński <szg.jasinski@gmail.com>
2024-07-21 21:12:05 +0200

    feat(core): initialize spring boot project 3.3.2
    Initialize spring boot project with basic dependencies for development
    purposes. Includes:
    - Lombok 1.18.34
    - Spring Boot DevTools
    - Spring Boot Web (spring-boot-starter-web)

---
+++ src/main/java/com/jasinski/resutomanager/ResutoManagerApplication.java
@@ -0,0 +1,13 @@
+package com.jasinski.resutomanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResutoManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ResutoManagerApplication.class, args);
+    }
+
+}
