19784a4598d5dd0c948c3d05d043c9f1d86b7baa
Szymon Jasiński <szg.jasinski@gmail.com>
2024-07-31 01:46:18 +0200

    refactor: add brackets to button onclick handler
    Rework onClick function to avoid confusing void expression.
    
    Signed-off-by: Szymon Jasiński <szg.jasinski@gmail.com>

--- client/vite-react/src/App.tsx
+++ client/vite-react/src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
@@ -18,7 +18,11 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button
+          onClick={() => {
+            setCount((count) => count + 1);
+          }}
+        >
           count is {count}
         </button>
         <p>
