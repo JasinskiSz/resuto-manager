03ffc700c3a23d60d0fccaed758fe63e0d853173
Szymon Jasiński <szg.jasinski@gmail.com>
2024-07-31 01:38:10 +0200

    chore: configure ts eslint to use strict config
    Strict config use with type checked.
    
    Signed-off-by: Szymon Jasiński <szg.jasinski@gmail.com>

--- client/vite-react/.eslintrc.cjs
+++ client/vite-react/.eslintrc.cjs
@@ -3,12 +3,16 @@ module.exports = {
   env: { browser: true, es2020: true },
   extends: [
     "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/strict-type-checked",
     "plugin:react-hooks/recommended",
     "prettier",
   ],
   ignorePatterns: ["dist", ".eslintrc.cjs"],
   parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: ["./tsconfig.json", "./tsconfig.node.json"],
+    tsconfigRootDir: __dirname,
+  },
   plugins: ["react-refresh"],
   rules: {
     "react-refresh/only-export-components": [
